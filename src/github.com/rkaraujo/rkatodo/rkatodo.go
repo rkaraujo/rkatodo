7b692b73b56efdcf5980985805cf7e8814b01db8
Renato Araujo <rkaraujo@gmail.com>
2015-08-14 00:33:40 -0300

    List of ToDos

--- src/github.com/rkaraujo/rkatodo/rkatodo.go
+++ src/github.com/rkaraujo/rkatodo/rkatodo.go
@@ -3,20 +3,29 @@ package main
 import (
 	"html/template"
 	"net/http"
+	"time"
+
+	"github.com/julienschmidt/httprouter"
 )
 
 type Todo struct {
 	Content string
+	Created time.Time
+}
+
+type ListForm struct {
+	TodoList []Todo
 }
 
 func main() {
-	http.HandleFunc("/", IndexHandler)
-	http.ListenAndServe(":8212", nil)
+	router := httprouter.New()
+	router.GET("/", ListHandler)
+	http.ListenAndServe(":8212", router)
 }
 
-func IndexHandler(rw http.ResponseWriter, r *http.Request) {
-	todo := &Todo{"Create a website"}
-
+func ListHandler(rw http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+	listForm := &ListForm{ []Todo{ Todo{"Create Website", time.Now()}, Todo{"Learn Go", time.Now()} } }
+	
 	tmpl, _ := template.ParseFiles("templates/index.html")
-	tmpl.Execute(rw, todo)
+	tmpl.Execute(rw, listForm)
 }
